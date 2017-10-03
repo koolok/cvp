042c8bd19e143e5657740f34e3610e43ad58658e
koolok <koolok69@gmail.com>
2017-10-03 13:13:33 +0200

    Adding comments Introduce multi-processing

--- extract_info.py
+++ extract_info.py
@@ -6,19 +6,24 @@ Created on Tue Sep 26 13:37:58 2017
 @author: koolok
 """
 
+from multiprocessing import Pool
 from lxml import etree
 import pandas as pd
 import os
 
+# Take a xml file and return tuple (name,pose,truncated,difficult,xmin,ymin,xmax,ymax)
 def parse (file) :
         
+    # Create xml tree from the file
     tree = etree.parse(file)
             
+    # Extract image informations
     filename = tree.xpath('/annotation/filename')[0].text
     width = int(tree.xpath('/annotation/size/width')[0].text)
     height = int(tree.xpath('/annotation/size/height')[0].text)
     depth = int(tree.xpath('/annotation/size/depth')[0].text)
     
+    #Extract object informations
     objects = list()
     
     for obj in tree.xpath('/annotation/object') :
@@ -35,19 +40,27 @@ def parse (file) :
         objects.append((name,pose,truncated,difficult,xmin,ymin,xmax,ymax))
         
         
+    #Transform objects informations in a Dataframe
     col_names = ['name', 'pose', 'truncated', 'difficult', 'xmin', 'ymin', 'xmax', 'ymax']
     obj_list = pd.DataFrame(objects,columns=(col_names))
 
     return (filename,width,height,depth,obj_list)
 
 
-img = list()
 
+# Create a pool of process
+pool = Pool()
+
+# Extract Images information
+files = list()
 for file in os.listdir("../VOC2007/Annotations") :
-    print(file)
-    img.append(parse("../VOC2007/Annotations/"+file))
+    files.append("../VOC2007/Annotations/"+file)
     
+img = pool.map_async(parse, files).get()
+    
+# Transfom the result in a Dataframe
 col_names = ['filename', 'with', 'height', 'depth', 'obj_list']
 images = pd.DataFrame(img,columns=(col_names))
 
+# Save the Dataframe
 images.to_pickle("images_dataframe.pk")
